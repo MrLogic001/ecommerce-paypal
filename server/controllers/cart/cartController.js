ecde46f34377e03e382141cb91cb31a10e453998
Peter Oluwatobi <oluwatobi015@gmail.com>
2025-09-11 14:28:30 +0100

    Corrected errors in models imported into controllers.

--- server/controllers/cart/cartController.js
+++ server/controllers/cart/cartController.js
@@ -1,4 +1,4 @@
-const Product = require("../../models/Product");
+const Product = require("../../models/product");
 const Cart = require("../../models/cart")
 
 const addToCart = async (req, res) => {
